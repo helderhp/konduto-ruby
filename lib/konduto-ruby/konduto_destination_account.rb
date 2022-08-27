99598fbaab7baa5727d6d4510368f8d4e1b13365
Helder Holanda Prezotto <helder@konduto.com>
2022-08-27 12:25:58 -0300

    adiciona o model para o konduto_destination_account.rb

---
+++ lib/konduto-ruby/konduto_destination_account.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class KondutoDestinationAccount < KondutoBase
+  attributes :amount, :bank_account, :bank_branch, :bank_code, :bank_name, :holder_name,
+             :holder_tax_id, :id, :key_value
+  attribute key_type: :symbol
+
+  KEY_TYPE = %i[pix_cpf pix_cnpj pix_phone pix_email pix_evp p2p none].freeze
+end
