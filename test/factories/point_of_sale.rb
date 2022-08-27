ff52b6fe6a0c76485d50c7421d4e1d6912bceaba
Helder Holanda Prezotto <helder@konduto.com>
2022-08-27 13:44:41 -0300

    adiciona os models para o nó point_of_sale

---
+++ test/factories/point_of_sale.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+FactoryGirl.define do
+  factory :konduto_point_of_sale do
+    id '000500'
+    name 'Capital Representações'
+    lat -23.5677666
+    long -46.6487763
+    address 'Rua Dez de Abril, 23'
+    city 'São Paulo'
+    state 'SP'
+    zip '01001-001'
+    country 'BR'
+  end
+end
