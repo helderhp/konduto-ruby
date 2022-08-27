11f4bef4990c3de072457cb90a7a5436023e7f8c
Helder Holanda Prezotto <helder@konduto.com>
2022-08-27 13:59:25 -0300

    adiciona os models para o nó vehicle

---
+++ lib/konduto-ruby/konduto_owner.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+
+class KondutoOwner < KondutoBase
+  attributes :tax_id, :name
+end
---
+++ test/factories/konduto_owner.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+FactoryGirl.define do
+  factory :konduto_owner do
+    tax_id '540.830.640-21'
+    name 'Cicero'
+  end
+end
